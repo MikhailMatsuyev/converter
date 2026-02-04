72844127bad279850403cdc38f55b3c8fb2abc4f
Mikhail Matsuyev <k501@yandex.ru>
2026-02-04 18:40:55 +0300

    Добаил в схему призма сущность File Применил миграцию

---
+++ backend/src/prisma/src/prisma/migrations/20260204150023_create_file_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "File" (
+    "id" TEXT NOT NULL,
+    "filename" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "storageKey" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
